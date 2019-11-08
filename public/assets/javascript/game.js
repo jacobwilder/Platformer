718d33b9f77a3348a516ba27db13a3b46a5b19c7
Christopher Mangin <mangin.christopher@gmail.com>
2019-11-08 10:32:30 -0500

    Implemented game logic/art assets

--- public/assets/javascript/game.js
+++ public/assets/javascript/game.js
@@ -1,24 +1,24 @@
 var cvs = document.getElementById("canvas");
 var ctx = cvs.getContext("2d");
 
-var bird = new Image();
+var dino = new Image();
 var bg = new Image();
 var fg = new Image();
 var pipeNorth = new Image();
 var pipeSouth = new Image();
 
-bird.src = "";
-bg.src = "";
+dino.src = "assets/images/dino1.gif";
+bg.src = "assets/images/zy85SqV.gif";
 fg.src = "";
-pipeNorth.src = "";
-pipeSouth.src = "";
+pipeNorth.src = "assets/images/pipe.png";
+pipeSouth.src = "assets/images/pipe2.png";
 
 // some variables
 
 var gap = 85;
 var constant = pipeNorth.height + gap;
 
-var bX = 10;
+var bX = 50;
 var bY = 150;
 
 var gravity = 1.5;
@@ -38,8 +38,8 @@ scor.src = "";
 document.addEventListener("keydown", moveUp);
 
 function moveUp() {
-    bY -= 25;
-    fly.play();
+  bY -= 25;
+  fly.play();
 }
 
 // pipe coordinates
@@ -47,58 +47,58 @@ function moveUp() {
 var pipe = [];
 
 pipe[0] = {
-    x: cvs.width,
-    y: 0
+  x: cvs.width,
+  y: 0
 };
-
 
 // draw images
 
 function draw() {
-    ctx.drawImage(bg, 0, 0);
+  ctx.drawImage(bg, 0, 0);
 
-    for (var i = 0; i < pipe.length; i++) {
-        ctx.drawImage(pipeNorth, pipe[i].x, pipe[i].y);
-        ctx.drawImage(pipeNorth, pipe[i].x, pipe[i].y + constant);
+  for (var i = 0; i < pipe.length; i++) {
+    ctx.drawImage(pipeNorth, pipe[i].x, pipe[i].y);
+    ctx.drawImage(pipeNorth, pipe[i].x, pipe[i].y + constant);
 
-        pipe[i].x--;
-        if (pipe[i].x == 125) {
-            pipe.push({
-                x: cvs.width,
-                y: Math.floor(Math.random() * pipeNorth.height) -
-                    pipeNorth.height
-            });
-        }
-
-        // detect collision
-
-        if (bX + bird.width >= pipe[i].x && bX <= pipe[i].x + pipeNorth.width &&
-            (bY <= pipe[i].y + pipeNorth.height || bY + bird.height >=
-                pipe[i].y + constant) || bY + bird.height >= cvs.height - fg.height) {
-            location.reload(); // reload the page
-        }
-
-        if (pipe[i].x == 5) {
-            score++;
-            scor.play();
-        }
-
-
+    pipe[i].x--;
+    if (pipe[i].x == 125) {
+      pipe.push({
+        x: cvs.width,
+        y: Math.floor(Math.random() * pipeNorth.height) - pipeNorth.height
+      });
     }
 
-    ctx.drawImage(pipeNorth, 100, 0);
-    ctx.drawImage(pipeSouth, 100, 0 + constant);
+    // detect collision
 
-    ctx.drawImage(fg, 0, cvs.height - fg.height);
-    ctx.drawImage(bird, bX, bY);
+    if (
+      (bX + dino.width >= pipe[i].x &&
+        bX <= pipe[i].x + pipeNorth.width &&
+        (bY <= pipe[i].y + pipeNorth.height ||
+          bY + dino.height >= pipe[i].y + constant)) ||
+      bY + dino.height >= cvs.height - fg.height
+    ) {
+      location.reload(); // reload the page
+    }
 
-    bY += gravity;
+    if (pipe[i].x == 5) {
+      score++;
+      scor.play();
+    }
+  }
 
-    ctx.fillStyle = "#000";
-    ctx.font = "20px Verdana";
-    ctx.fillText("Score : " + score, 10, cvs.height - 20);
+  // ctx.drawImage(pipeNorth, 100, 0);
+  // ctx.drawImage(pipeSouth, 100, 0 + constant);
 
-    requestAnimationFrame(draw);
+  ctx.drawImage(fg, 0, cvs.height - fg.height);
+  ctx.drawImage(dino, bX, bY);
+
+  bY += gravity;
+
+  ctx.fillStyle = "#000";
+  ctx.font = "20px Verdana";
+  ctx.fillText("Score : " + score, 10, cvs.height - 20);
+
+  requestAnimationFrame(draw);
 }
 
 draw();
